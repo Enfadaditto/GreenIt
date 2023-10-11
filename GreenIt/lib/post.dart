ca7f6dc9c36ac0771d770ec7de2c50c3f7e5577c
HAIBGOM <haibgom@etsinf.upv.es>
2023-10-11 19:08:12 +0200

    some things added, now time to get the object

--- GreenIt/lib/post.dart
+++ GreenIt/lib/post.dart
@@ -5,23 +5,35 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _searchPressed() {
+      //TODO
+      print("SEARCH PRESSED");
+    }
+
+    void _menuPressed() {
+      //TODO
+      print("MENU PRESSED");
+    }
+
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        actions: const <Widget>[
-          Icon(Icons.search, color: Colors.white),
-          SizedBox(width: 16),
-          Icon(Icons.menu, color: Colors.white),
-          SizedBox(width: 16)
+        actions: <Widget>[
+          IconButton(
+              onPressed: _searchPressed,
+              icon: const Icon(Icons.search, color: Colors.white)),
+          IconButton(
+              onPressed: _menuPressed,
+              icon: const Icon(Icons.menu, color: Colors.white)),
         ],
         title: const Text(
           "Page Title",
           style: TextStyle(color: Colors.white),
         ),
         bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(20),
+          preferredSize: Size.fromHeight(15),
           child: Text(
-            "Subtitulo",
+            "Subtitle",
             style: TextStyle(color: Colors.white70),
           ),
         ),
@@ -43,9 +55,55 @@ class PostDetailed extends StatefulWidget {
 class _PostDetailedState extends State<PostDetailed> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [],
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 25),
+          SizedBox(
+            height: 500,
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              children: const <Widget>[
+                StepCard('Description Red', Colors.red),
+                StepCard('Description Blue', Colors.blue),
+                StepCard('Description Green', Colors.green),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class StepCard extends StatelessWidget {
+  final String text;
+  // final Image image;
+  final Color color;
+
+  const StepCard(this.text, this.color, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+          height: 500,
+          //child: Image(),
+          color: color,
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ],
     );
   }
 }
