50904e438b623bf3cf3946eaec311a0836f3fb7b
HAIBGOM <haibgom@etsinf.upv.es>
2023-10-16 20:32:30 +0200

    merge acted weirdly

--- GreenIt/lib/main.dart
+++ GreenIt/lib/main.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 import 'dart:ffi';
 import 'ServerConnect.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/pages/profile.dart';
-import 'package:my_app/widgets/bottom_navigation_bar_widget.dart';
 import 'post.dart';
 
 void main() {
@@ -15,19 +13,59 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   const MyApp({super.key});
-  
+
   // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   final serverUrl = "http://localhost:8080/user?email=ida@mail.com";
-  
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -36,18 +74,26 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blue.shade300,
-        dividerColor: Colors.black,
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -69,27 +115,25 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             FutureBuilder<String>(
-            future: ServerConnect().fetchData(serverUrl),
-            // builder: (context, snapshot) {
-            //   if (snapshot.connectionState == ConnectionState.waiting) {
-            //     return CircularProgressIndicator();
-            //   } else if (snapshot.hasError) {
-            //     return Text('Error: ${snapshot.error}');
-            //   } else {
-            //     return Text('Fetched Data: ${snapshot.data}');
-            //   }
-            // }),
-            builder:(context, snapshot) {
-              if(snapshot.hasError){
-                return Text('Error: ${snapshot.error}');
-              }
-              else if (snapshot.hasData){
-                return Text('Fetched Data: ${snapshot.data}');
-              }
-              else {
-                return Text('Unknown error');
-              }
-            }),
+                future: ServerConnect().fetchData(serverUrl),
+                // builder: (context, snapshot) {
+                //   if (snapshot.connectionState == ConnectionState.waiting) {
+                //     return CircularProgressIndicator();
+                //   } else if (snapshot.hasError) {
+                //     return Text('Error: ${snapshot.error}');
+                //   } else {
+                //     return Text('Fetched Data: ${snapshot.data}');
+                //   }
+                // }),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    return Text('Fetched Data: ${snapshot.data}');
+                  } else {
+                    return Text('Unknown error');
+                  }
+                }),
             const Text(
               'You have pushed the button this many times:',
             ),
@@ -100,6 +144,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
