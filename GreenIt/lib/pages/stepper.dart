b171f561e57381c5394a3a2902471304e2817447
José Ramón <jrber23@gmail.com>
2023-11-21 20:06:40 +0100

    decoracion stepper

--- GreenIt/lib/pages/stepper.dart
+++ GreenIt/lib/pages/stepper.dart
@@ -13,19 +13,18 @@ class _MyStepper extends State<MyStepper> {
     return Timeline.tileBuilder(
       shrinkWrap: true,
       padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-      scrollDirection: Axis.vertical,
+      scrollDirection: Axis.horizontal,
       builder: TimelineTileBuilder.fromStyle(
-        oppositeContentsBuilder: (context, index) => index % 2 == 0 ? Text("Prueba $index", style: TextStyle(color: Colors.white)) : null,
-        itemExtent: 50.0,
+        oppositeContentsBuilder: (context, index) => index % 2 == 0 ? Padding(padding: EdgeInsets.all(8.0), child: Text("Prueba $index", style: TextStyle(color: Colors.white))) : null,
+        itemExtent: 75.0,
         indicatorStyle: IndicatorStyle.dot,
         addRepaintBoundaries: true,
         endConnectorStyle: ConnectorStyle.transparent,
         contentsAlign: ContentsAlign.basic,
         connectorStyle: ConnectorStyle.solidLine,
-        contentsBuilder: (context, index) => index % 2 != 0 ? Text("Prueba $index", style: TextStyle(color: Colors.white)) : null,
+        contentsBuilder: (context, index) => index % 2 != 0 ? Padding(padding: EdgeInsets.all(8.0), child: Text("Prueba $index", style: TextStyle(color: Colors.white))) : null,
         itemCount: 15
       ),
-      // TimelineTheme(child: ,)
     );
   }
 }
